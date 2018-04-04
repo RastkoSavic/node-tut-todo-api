ef219e08524f2a9193c823faa0cd9a390fb13c34
RastkoSavic <rastkosavic23@gmail.com>
2018-04-04 12:00:29 +0200

    =Add GET /todos route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,8 +1,8 @@
 const expect = require('expect');
 const request = require('supertest');
 
-const {app} = require('./../server');
-const {Todo} = require('./../models/todo');
+const { app } = require('./../server');
+const { Todo } = require('./../models/todo');
 
 beforeEach((done) => {
     Todo.remove({}).then(() => done());
@@ -14,7 +14,7 @@ describe('POST /todos', () => {
 
         request(app)
             .post('/todos')
-            .send({text})
+            .send({ text })
             .expect(200)
             .expect((res) => {
                 expect(res.body.text).toBe(text);
@@ -40,7 +40,7 @@ describe('POST /todos', () => {
             .send({})
             .expect(400)
             .end((err, res) => {
-                
+
                 if (err) {
                     return done(err);
                 }
@@ -49,7 +49,7 @@ describe('POST /todos', () => {
                     expect(todos.length).toBe(0);
                     done();
                 }).catch((e) => done(e));
-                
+
             });
     });
 });
