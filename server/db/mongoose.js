e03577d2c491d3c9e4167d39d11f8e4944ba64f2
RastkoSavic <rastkosavic23@gmail.com>
2018-04-04 17:51:01 +0200

    =Mlab

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,6 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/TodoApp');
+mongoose.connect('mongodb://<RastkoS>:<mrasha23lab87>@ds135179.mlab.com:35179/todo_api' || 'mongodb://127.0.0.1:27017/TodoApp');
 
 module.exports = { mongoose };
