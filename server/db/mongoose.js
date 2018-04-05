66eb174641ba31bad4a33e70bb271a8eca8e1ab1
RastkoSavic <rastkosavic23@gmail.com>
2018-04-06 01:41:34 +0200

    =Added test for DELETE /todos/:id route

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,8 +2,8 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
+// 'mongodb://RastkoS:mrasha23lab87@ds135179.mlab.com:35179/todo_api' || 
 
-
-mongoose.connect('mongodb://RastkoS:mrasha23lab87@ds135179.mlab.com:35179/todo_api' || 'mongodb://127.0.0.1:27017/TodoApp');
+mongoose.connect('mongodb://127.0.0.1:27017/TodoApp');
 
 module.exports = { mongoose };
