f3e81087f88f367f48817462720ae787b69125b9
RastkoSavic <rastkosavic23@gmail.com>
2018-04-08 18:40:31 +0200

    =Update config to use json

--- server/config/config.js
+++ server/config/config.js
@@ -1,9 +1,18 @@
 var env = process.env.NODE_ENV || 'developement';
 
-if (env === 'developement') {
-    process.env.port = 3000;
-    process.env.MONGODB_URI = 'mongodb://127.0.0.1:27017/TodoApp';
-} else if (env === 'test') {
-    process.env.port = 3000;
-    process.env.MONGODB_URI = 'mongodb://127.0.0.1:27017/TodoAppTest';
+if (env === 'developement' || env === 'test') {
+    var config = require('./config.json');
+    var envConfig = config[env];
+
+    Object.keys(envConfig).forEach((key) => {
+        process.env[key] = envConfig[key];
+    });
 }
+
+// if (env === 'developement') {
+//     process.env.port = 3000;
+//     process.env.MONGODB_URI = 'mongodb://127.0.0.1:27017/TodoApp';
+// } else if (env === 'test') {
+//     process.env.port = 3000;
+//     process.env.MONGODB_URI = 'mongodb://127.0.0.1:27017/TodoAppTest';
+// }
